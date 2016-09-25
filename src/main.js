dbc95e3dc6eef69cc8026bc4c8670b6a485a6c1a
FelipeMonobe <xinube@gmail.com>
2016-09-24 23:40:48 -0300

    DOM reactive to http trigged by user

--- src/main.js
+++ src/main.js
@@ -1,28 +1,52 @@
 import {run} from '@cycle/xstream-run';
-import {div, label, input, hr, h1, makeDOMDriver} from '@cycle/dom';
+import {button, div, img, makeDOMDriver} from '@cycle/dom';
+import {makeHTTPDriver} from '@cycle/http';
 
 /**
-* @desc Bootstraps the application
-* @private
-* @function main
-* @param {object} sources object containing drivers to receive input
-* @return {object} sinks object containing streams to output
-*/
+ * @desc Bootstraps the application
+ * @private
+ * @function main
+ * @param {object} sources object containing drivers to receive input
+ * @return {object} sinks object containing streams to output
+ */
 function main(sources) {
-  const sinks = {
-    DOM: sources.DOM.select('.field').events('input')
-      .map(ev => ev.target.value)
-      .startWith('')
-      .map(name =>
-        div([
-          label('Name: '),
-          input('.field', {attrs: {type: 'text'}}),
-          hr(),
-          name.trim() ? h1(`Hello ${name}!!`) : h1()
+  const buttonClick$ = sources.DOM
+    .select('.button')
+    .events('click');
+
+  const getRandomGiphy$ = buttonClick$
+    .map(() => {
+      return {
+        url: 'http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC',
+        category: 'gif'
+      };
+    });
+
+  const getRandomGiphyData$ = sources.HTTP
+    .select('gif')
+    .flatten()
+    .map(res => res.body)
+    .startWith(null);
+
+  const vdom$ = getRandomGiphyData$
+    .map(gif =>
+      div('.content', [
+        button('.button', 'Randomize'),
+        gif === null ? null : div('.gif-details', [
+          img({attrs: {src: gif.data.fixed_width_downsampled_url}})
         ])
-      )
+      ])
+    );
+
+  const sinks = {
+    DOM: vdom$,
+    HTTP: getRandomGiphy$
   };
+
   return sinks;
 }
 
-run(main, {DOM: makeDOMDriver('#app')});
+run(main, {
+  DOM: makeDOMDriver('#app'),
+  HTTP: makeHTTPDriver()
+});
